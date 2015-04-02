6c6a717d3c8238c64ab22aba643bc713b8852ed6
Ivan Montiel <idmontie@asu.edu>
2015-04-02 09:58:19 -0700

    change build. Track minified files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function( grunt ) {
 	var SOURCE_DIR = 'src/';
-	var BUILD_DIR = 'build/';
+	var BUILD_DIR = 'src/';
 	var SVN = 'https://plugins.svn.wordpress.org';
 
 	require( 'matchdep' ).filterDev( ['grunt-*'] ).forEach( grunt.loadNpmTasks );
@@ -11,34 +11,6 @@ module.exports = function( grunt ) {
 			return [ pkg.author ].concat( pkg.contributors ).map( function( person ) {
 				return person.wp.username;
 			} ).join( ', ' );
-		},
-		clean: {
-			all: [ BUILD_DIR ],
-			svn: [ BUILD_DIR + '*', '!' + BUILD_DIR + '.svn' ]
-		},
-		copy: {
-			all: {
-				expand: true,
-				cwd: SOURCE_DIR,
-				src: '**',
-				dest: BUILD_DIR
-			},
-			templates: {
-				options: {
-					process: grunt.template.process
-				},
-				expand: true,
-				cwd: SOURCE_DIR,
-				src: [
-					'plugin.php',
-					'readme.txt'
-				],
-				dest: BUILD_DIR
-			},
-			pkg: {
-				src: 'package.json',
-				dest: BUILD_DIR
-			}
 		},
 		jshint: {
 			options: grunt.file.readJSON( '.jshintrc' ),
@@ -67,14 +39,6 @@ module.exports = function( grunt ) {
 				src: BUILD_DIR + 'js/*.js'
 			}
 		},
-		svn: {
-			checkout: {
-				cmd: 'checkout ' + SVN + '/<%= pkg.name %>/trunk ' + BUILD_DIR
-			},
-			tag: {
-				cmd: 'copy ' + SVN + '/<%= pkg.name %>/trunk ' + SVN + '/<%= pkg.name %>/tags/<%= pkg.version %> -m "<%= pkg.version %>"'
-			}
-		},
 		uglify: {
 			all: {
 				expand: true,
@@ -97,5 +61,5 @@ module.exports = function( grunt ) {
 	} );
 
 	grunt.registerTask( 'default', [ 'jshint' ] );
-	grunt.registerTask( 'build', [ 'clean:all', 'svn:checkout', 'clean:svn', 'copy', 'uglify', 'jsvalidate' ] );
+	grunt.registerTask( 'build', [ 'uglify', 'jsvalidate' ] );
 };
