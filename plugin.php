c0140a82699ccdcbfa391ef8a439e4a1d18c7d8f
Nagarjuna Myla <chasethenag420@gmail.com>
2016-02-05 09:33:21 -0700

    Increase version number to 1.2.0

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /* Plugin Name:       Front-end Editor
  * Plugin URI:        https://wordpress.org/plugins/wp-front-end-editor/
  * Description:       Edit your posts on the front-end of your site.
- * Version:           1.0.7
+ * Version:           1.2.0
  * Author:            Ella Iseulde Van Dorpe
  * Author URI:        http://iseulde.com
  * License:           GPL-2.0+
